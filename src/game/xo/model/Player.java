6ebcbb30cf9408d36245f8cc28dea4c9fafb158d
fettqa <fettqa@gmail.com>
2020-01-26 23:09:25 +0300

    Create Player

---
+++ src/game/xo/model/Player.java
@@ -0,0 +1,19 @@
+package game.xo.model;
+
+public class Player {
+    private final String name;
+    private final Figure figure;
+
+    public Player(String name, Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
