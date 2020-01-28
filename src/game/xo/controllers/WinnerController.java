a60f275c112ff1502ee2979c1e36a22b37e42c78
fettqa <fettqa@gmail.com>
2020-01-29 00:02:49 +0300

    New version of the WinnerController(recursion)

---
+++ src/game/xo/controllers/WinnerController.java
@@ -0,0 +1,60 @@
+package game.xo.controllers;
+
+import game.xo.model.Field;
+import game.xo.model.Figure;
+import game.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class WinnerController {
+
+    public Figure getWinner(final Field field) {
+
+        try {
+            for (int i =0; i< 3; i++) {
+                if (checkWinnerCondition(field, new Point(i, 0), p -> new Point(p.x, p.y + 1))) {
+                    return field.getFigure(new Point(i, 0));
+                }
+            }
+            for (int i =0; i< 3; i++) {
+                if (checkWinnerCondition(field, new Point(i, 0), p -> new Point(p.x + 1, p.y))) {
+                    return field.getFigure(new Point(i, 0));
+                }
+            }
+            if (checkWinnerCondition(field, new Point(0, 0), p -> new Point(p.x + 1, p.y + 1))) {
+                return field.getFigure(new Point(0, 0));
+            }
+            if (checkWinnerCondition(field, new Point(0, 0), p -> new Point(p.x + 1, p.y - 1))) {
+                return field.getFigure(new Point(1, 1));
+            }
+        } catch (InvalidPointException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private boolean checkWinnerCondition (final Field field,
+                                          final Point currentPoint,
+                                          final IPointGenerator pointGenerator) {
+        final Figure currentFigure;
+        final Figure nextFigure;
+        final Point nextPoint = pointGenerator.next(currentPoint);
+        try {
+            currentFigure = field.getFigure(currentPoint);
+            nextFigure = field.getFigure(nextPoint);
+        } catch (final InvalidPointException e) {
+            return true;
+        }
+
+        if (currentFigure == null) return false;
+
+        if (currentFigure != nextFigure) return false;
+
+        return checkWinnerCondition(field, nextPoint, pointGenerator);
+
+    }
+
+    private interface IPointGenerator {
+        public Point next(final Point point);
+    }
+}
